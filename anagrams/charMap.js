0d466aa0248e0688941ffd25987a8922a4488f73
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-12-18 20:05:09 +0300

    charmap method done

--- anagrams/charMap.js
+++ anagrams/charMap.js
@@ -1,21 +1,23 @@
 function anagram(stringA, stringB) {
   const aCharMap = buildCharMap(stringA);
   const bCharMap = buildCharMap(stringB);
-  if (aCharMap === bCharMap) {
-    return true;
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false;
   }
-  return false;
+  for (let key of Object.keys(aCharMap)) {
+    if (aCharMap[key] !== bCharMap[key]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 function buildCharMap(str) {
   const charMap = {};
-
-  for (let char of str.replace(/[^\w]/g, "".toLowerCase())) {
-    console.log(str)
+  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
     charMap[char] = charMap[char] + 1 || 1;
-    console.log(charMap)
-    return charMap;
   }
+  return charMap;
 }
 
 console.log(anagram("fun", "Ufn"));
