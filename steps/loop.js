b738542122126e67269a2e7b31e15629f74c92bf
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-12-18 20:54:45 +0300

    using repeat method and a loop

--- steps/loop.js
+++ steps/loop.js
@@ -1,6 +1,9 @@
 function steps(n) {
   for (i = 1; i <= n; i++) {
-    console.log("#".repeat(i) + " ".repeat(n - i));
+    console.log("#".repeat(i));
+    for (j = n - 1; j >= 0; j--) {
+      console.log("*".repeat(j));
+    }
   }
 }
 
